caeb547a2006e4f87d53e3515e62fe915586fe69
Zbigniew Siciarz <antyqjon@gmail.com>
2012-08-18 18:24:59 +0200

    static -> public

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import os
 from flask import Flask, render_template
 
-app = Flask(__name__)
+PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
+
+app = Flask(
+    __name__,
+    static_folder=os.path.join(PROJECT_ROOT, 'public'),
+    static_url_path='/public'
+)
 
 @app.route('/')
 def hello():
