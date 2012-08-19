326e5be50cc5555cb934204c4a5704e5eb0fee05
Zbigniew Siciarz <antyqjon@gmail.com>
2012-08-19 14:41:58 +0200

    First step towards modularization using require.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,30 @@
-(function($) {
-    $(function() {
-        var sun = new Sun();
-        var sunView = new SunView({
-            model: sun
-        });
+requirejs.config({
+    shim: {
+        'libs/jquery': {
+            exports: '$'
+        },
+        'libs/underscore': {
+            exports: '_'
+        },
+        'libs/backbone': {
+            deps: ['libs/underscore', 'libs/jquery'],
+            exports: 'Backbone'
+        }
+    }
+});
 
-        sun.fetch();
-    });
-})(jQuery);
+define(
+    ['libs/jquery', 'libs/underscore', 'libs/backbone'],
+    function($, _, Backbone) {
+        $(function() {
+            /*
+            var sun = new Sun();
+            var sunView = new SunView({
+                model: sun
+            });
+
+            sun.fetch();
+            */
+        });
+    }
+);
